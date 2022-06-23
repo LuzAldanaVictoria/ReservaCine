9e60b074aecaaad22d873607e54fb2fca6b011c4
LuzVictoria <luzaldanavictoria@gmail.com>
2022-06-22 21:49:55 -0300

    Revision paso a paso ReservasController

--- Grupo3.ReservaDeCine/Controllers/ReservasController.cs
+++ Grupo3.ReservaDeCine/Controllers/ReservasController.cs
@@ -15,28 +15,31 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Grupo3.ReservaDeCine.Controllers
 {
-    [Authorize]
+    [Authorize] // Solo pueden acceder personas autenticadas
+
+
+    //Esto es la inyeccionde de dependencia,   es automatico cuando hago el scafolding
     public class ReservasController : Controller
     {
-        private readonly CineDbContext _context;
+        private readonly CineDbContext _context; // Es muy importante que sea solo “readonly”
 
-        public ReservasController(CineDbContext context)
+        public ReservasController(CineDbContext context) // cuando creo el constructor, recibe el contexto de la base de datos( DbContext) para despues usarlo
         {
-            _context = context;
+            _context = context;// recibo el contexto y lo guardo en esta variable
         }
 
         [HttpGet]
         [Authorize(Roles = nameof(Role.Administrador))]
-        public IActionResult Index()
+        public IActionResult Index() // el IactionResult, es una “interfaz”
         {
-            var reservas = _context
+            var reservas = _context //Pido el contexto de reserva,“accedo al set de datos de reservas”
                  .Reservas
-                 .Include(x => x.Cliente)
-                 .Include(x => x.Funcion).ThenInclude(x => x.Pelicula)
-                 .Include(x => x.Funcion).ThenInclude(x => x.Sala)
-                 .ToList();
+                 .Include(x => x.Cliente) //Pido los clientes de reserva
+                 .Include(x => x.Funcion).ThenInclude(x => x.Pelicula) // Pido las funciones y que de las funciones me traiga las peliculas
+                 .Include(x => x.Funcion).ThenInclude(x => x.Sala) // / De la funcion, pido que me inluya la sala
+                 .ToList(); // Me lo presenta en forma de lista
 
-            return View(reservas);
+            return View(reservas); // Le mando a la vista la lista de reservas
         }
 
 
@@ -67,19 +70,23 @@ namespace Grupo3.ReservaDeCine.Controllers
         [Authorize(Roles = nameof(Role.Cliente))]
         public IActionResult MisReservas()
         {
-            int clienteId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value); 
+            int clienteId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value); // levante el id del usuario que esta autenticado.
 
+            // la pantalla de mis reservas necesita la lista de mis reservas, que se hace asi:
             var reservas = _context
                 .Reservas
                 .Include(x => x.Funcion).ThenInclude(x => x.Pelicula)
                 .Include(x => x.Funcion).ThenInclude(x => x.Sala)
-                .Where(reserva => reserva.ClienteId == clienteId && reserva.Funcion.Fecha >= DateTime.Today)
+                .Where(reserva => reserva.ClienteId == clienteId && reserva.Funcion.Fecha >= DateTime.Today) 
                 .ToList();
 
-            return View(reservas);
+            return View(reservas); // retorna a la vista y le pasa la lista de mis reservas
         }
 
 
+
+
+        // Entrando por cartelera, seleccionando pelicula y luego reservar para una funcion determinada
         [HttpGet]
         [Authorize(Roles = nameof(Role.Cliente))]
         public IActionResult CrearReservaPorFuncion(int? id)
@@ -127,22 +134,24 @@ namespace Grupo3.ReservaDeCine.Controllers
             if (funcion == null)
                 ModelState.AddModelError(nameof(Reserva.Funcion), "La función no se encuentra disponible");
 
-            ValidarCantButacas(reserva, funcion);
+            ValidarCantButacas(reserva, funcion); // función que verifica si me alcanzan la cantidad de butacas disponibles para hacer la reserva y que no sea mayor a 10
 
             if (ModelState.IsValid)
             {
-                reserva.FechaDeAlta = DateTime.Now;
-                funcion.CantButacasDisponibles -= reserva.CantButacas;
-                reserva.CostoTotal = reserva.CantButacas * funcion.Sala.Tipo.PrecioEntrada;
+                reserva.FechaDeAlta = DateTime.Now; // setea la fecha de alta de reserva al día de hoy
+                funcion.CantButacasDisponibles -= reserva.CantButacas; // resta de las disponibles, las bustacas de la reserva
+                reserva.CostoTotal = reserva.CantButacas * funcion.Sala.Tipo.PrecioEntrada; ; // calcula el costo de la reserva
                 reserva.ClienteId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);  // aca le asigna a la reserva el clienteId con el claims de Name Identifier
-                _context.Add(reserva);
-                _context.SaveChanges();
-                return RedirectToAction(nameof(MisReservas));
+                _context.Add(reserva); // agrega la reserva a la base de datos
+                _context.SaveChanges();  //guardo cambios en db
+                return RedirectToAction(nameof(MisReservas));// Si el modelo es valido, me redirige a MIS RESERVAS
             }
 
-            ViewData["CostoEntrada"] = funcion.Sala.Tipo.PrecioEntrada;
-            ViewData["Peliculas"] = new SelectList(_context.Peliculas, "Id", "Nombre", funcion.PeliculaId);
-            ViewData["FechaHora"] = new SelectList(_context.Funciones, "Id", "FechaHora", funcion.FechaHora);
+
+            //Si el modelo no es valido ?
+            ViewData["CostoEntrada"] = funcion.Sala.Tipo.PrecioEntrada; //De las funciones pido que me haga una lista mostrando el ID y la FechaHora
+            ViewData["Peliculas"] = new SelectList(_context.Peliculas, "Id", "Nombre", funcion.PeliculaId); //aca hago un view data, que después voy a usar en la vista para saber el nombre de la película
+            ViewData["FechaHora"] = new SelectList(_context.Funciones, "Id", "FechaHora", funcion.FechaHora); //aca hago un view data, que después voy a usar en la vista para saber la fecha y hora de las funcions
 
             reserva.Funcion = funcion;
 
@@ -174,7 +183,7 @@ namespace Grupo3.ReservaDeCine.Controllers
         }
 
         [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
+        [ValidateAntiForgeryToken] //###  //verificar que es esta linea
         [Authorize(Roles = nameof(Role.Administrador))]
         public  IActionResult DeleteConfirmed(int id)
         {
